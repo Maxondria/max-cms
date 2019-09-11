c6a305cc8bc813e635f7b009905b6e709813b84d
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-09-11 13:04:16 +0300

    [task] Added a delete category modal and functionality

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -17,13 +17,61 @@
                 @foreach($categories as $category)
                     <tr>
                         <td>{{$category->name}}</td>
-                        <td><a href="{{route('categories.edit', $category->id)}}" class="btn btn-info btn-sm">Edit</a>
+                        <td>
+                            <a href="{{route('categories.edit', $category->id)}}" class="btn btn-info btn-sm">Edit</a>
+                            <button class="btn btn-danger btn-sm" onclick="handleDelete({{ $category->id }})">Delete
+                            </button>
                         </td>
                     </tr>
                 @endforeach
                 </tbody>
             </table>
+
+            <!-- Modal -->
+            <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel"
+                 aria-hidden="true">
+                <div class="modal-dialog" role="document">
+
+                    <form action="" method="post" id="deleteCategoryForm">
+
+                        @csrf
+                        @method('DELETE')
+
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="deleteModal">Delete Category</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <p class="text-center text-bold">
+                                    Are you sure you want to delete this category?
+                                </p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">No, Close</button>
+                                <button type="submit" class="btn btn-danger">Yes, Delete</button>
+                            </div>
+                        </div>
+
+                    </form>
+
+                </div>
+            </div>
+
         </div>
     </div>
+@endsection
+
+@section('scripts')
+    <script>
+        function handleDelete(categoryId) {
+            const form = document.getElementById('deleteCategoryForm');
+            form.action = '/categories/' + categoryId;
+
+            $('#deleteModal').modal('show');
+        }
+    </script>
 @endsection
 
