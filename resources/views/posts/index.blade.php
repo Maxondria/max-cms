6f33ce03e7bd0be4d3fb996238c5165e8cd486f3
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-09-11 22:26:05 +0300

    [chore] Added 'php artisan storage:link' to move the protected images storage to the public folder

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,7 +8,21 @@
     <div class="card card-default">
         <div class="card-header">Posts</div>
         <div class="card-body">
+            <table class="table">
+                <thead>
+                <th>Image</th>
+                <th>Title</th>
+                </thead>
+                <tbody>
+                @foreach($posts as $post)
+                    <tr>
+                        <td>{{$post->image}}</td>
+                        <td>{{$post->title}}</td>
+                    </tr>
+                @endforeach
 
+                </tbody>
+            </table>
         </div>
     </div>
 @endsection
